70ffbe3af575ac6107b07fe0a313ab4aac15b953
Greg C <gregcourt10@gmail.com>
2023-01-24 15:32:52 +0000

    first commit

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class BankAccount {
+    String firstName;
+    String lastName;
+    LocalDate dateOfBirth;
+    int accountNumber;
+    int balance = 0;
+
+
+    
+}
